16033f4fc71e55433cbca2b9eb26face7962205a
Dana Salk <46821074+danajsalk@users.noreply.github.com>
2022-08-25 09:32:58 -0500

    Create S3_read_csv.py

---
+++ S3/S3_read_csv.py
@@ -0,0 +1,29 @@
+import boto3
+import codecs
+import csv
+import pandas as pd
+
+#boto3.setup_default_session(profile_name='cp-aiops-dev')
+
+bucket                 = 'BUCKET-NAME-HERE'
+prefix                 = 'PREFIX/GOES/HERE'
+
+s3c = boto3.client("s3")
+
+
+my_bucket = s3_resource.Bucket(bucket)
+
+all_dat = []
+objs = [obj.key for obj in my_bucket.objects.filter(Prefix=prefix)]
+
+ct = 0
+for obj in my_bucket.objects.filter(Prefix=prefix):
+    if (obj.key).endswith('.csv'):
+        ct += 1
+        print(ct,'/', len(objs), ':', obj.key)
+        data = s3c.get_object(Bucket=bucket, Key=obj.key)
+
+        for row in csv.DictReader(codecs.getreader("utf-8")(data["Body"])):
+            all_dat.append(row)
+        
+df = pd.DataFrame(all_dat)
